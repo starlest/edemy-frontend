f20d0b883692b3ea76432efa59ee67def3012c28
Edwin Chia <starlest@hotmail.com>
2017-01-11 01:04:36 +0700

    Lots of work done on side nav, home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import {Component} from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	title: string = "Home";
+	windowWidth: any;
+
+	constructor(private ngZone: NgZone) {
+		this.windowWidth = window.innerWidth;
+		window.onresize = (e) => {
+			ngZone.run(() => {
+				this.windowWidth = window.innerWidth;
+			});
+		};
+	}
+
+	shouldSideNavBeHidden(): boolean {
+		return this.windowWidth > 1000;
+	}
+
+	currentSideNavMode(): string {
+		return this.windowWidth > 1000 ? 'side' : 'over';
+	}
 }
