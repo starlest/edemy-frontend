9398e9329f4bddfbd7609cdfa5e71034fe2a52f4
Edwin Chia <starlest@hotmail.com>
2017-01-10 14:31:24 +0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    this.http.request(
+      'http://sample-env-3.etaufnmjkp.ap-southeast-1.elasticbeanstalk.com/api/values')
+      .subscribe((res: Response) => {
+        this.title = res.json();
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    FlexLayoutModule.forRoot(),
+    MaterialModule.forRoot(),
     HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
