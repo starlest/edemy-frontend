167cf75d78c970abe18d751e59836cd7af550ba5
Edwin Chia <starlest@gmail.com>
2017-01-14 01:58:38 +0800

    Fix bug where side nav is always locked open at the start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component, NgZone, ChangeDetectionStrategy} from '@angular/core';
+import {
+  Component, NgZone, ChangeDetectionStrategy, OnInit
+} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Store} from '@ngrx/store';
 import * as fromRoot from './reducers';
@@ -10,13 +12,17 @@ import * as layout from './actions/layout';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isSidenavLockedOpen$: Observable<boolean>;
 
   constructor(private store: Store<fromRoot.State>,
               private ngZone: NgZone) {
     this.isSidenavLockedOpen$ = store.select(fromRoot.isSidenavLockedOpen);
     this.setUpWindowOnResizeListener()
+  }
+
+  ngOnInit() {
+    this.isSidenavLockedOpen$ = Observable.of(window.innerWidth > 1600 ? true : false);
   }
 
   setUpWindowOnResizeListener() {
